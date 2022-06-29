0ca0c1357fb78fc09b2959d966c631e11f640a85
CodeSpec <98212758+CodeSpec01@users.noreply.github.com>
2022-06-29 17:37:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,103 @@
+# Importing Turtle Module -----------------------------------------------------------------------------------------------
+import turtle
+
+# Creating the Window -------------------------------------------------------------------------------------------------
+
+wn = turtle.Screen()
+wn.bgcolor("red")
+wn.title("Turtle")
+m = turtle.Turtle()
+
+# Starting of M -------------------------------------------------------------------------------------------------------------------
+
+m.penup()
+m.goto(-600,-100)
+m.pendown()
+m.left(90)
+m.forward(200)
+m.right(90)
+m.forward(40)
+m.right(65)
+m.forward(130)
+m.left(130)
+m.forward(130)
+m.right(65)
+m.forward(40)
+m.right(90)
+m.forward(200)
+m.penup()
+m.right(90)
+m.forward(190)
+m.pendown()
+m.backward(40)
+m.right(90)
+m.forward(118)
+m.right(155)
+m.forward(130)
+m.left(130)
+m.forward(130)
+m.right(155)
+m.forward(118)
+m.left(90)
+m.forward(40)
+
+# Going for the A -----------------------------------------------------------------------------------------------------------------------------------
+
+m.penup()
+m.forward(40)
+m.pendown()
+m.left(80)
+m.forward(200)
+m.right(80)
+m.forward(80)
+m.right(80)
+m.forward(200)
+m.right(100)
+m.forward(40)
+m.right(80)
+m.forward(35)
+m.left(80)
+m.forward(55)
+m.left(80)
+m.forward(35)
+m.right(80)
+m.forward(40)
+m.penup()
+m.backward(108)
+m.right(80)
+m.forward(35)
+m.forward(40)
+m.pendown()
+m.forward(65)
+m.left(80)
+m.forward(20)
+m.left(80)
+m.forward(65)
+m.left(100)
+m.forward(42)
+
+# Going For the R -------------------------------------------------------------------------------------------------------------------------------
+
+m.penup()
+m.forward(40)
+m.right(80)
+m.forward(75)
+m.left(80)
+m.forward(40)
+m.pendown()
+m.left(90)
+m.forward(200)
+
+# Going for the V -----------------------------------------------------------------------
+
+
+
+# Going for the E -------------------------------------------------------------------------
+
+
+
+# Going fot the L ------------------------------------------------------------------------
+
+
+
+turtle.done()
